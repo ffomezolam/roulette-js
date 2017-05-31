904b3d1a58f9b7a9f23f7198b6e95fe56b40b714
ffomezolam <ffomezolam@gmail.com>
2017-05-31 19:49:54 -0400

    more thorough testing and updates

--- roulette.js
+++ roulette.js
@@ -23,7 +23,8 @@
         return {
             item: item,
             tally: tally,
-            probability: probability
+            probability: probability,
+            pcum: 0
         }
     }
 
@@ -71,221 +72,265 @@
         this._total = 0;
         this._compare = defaultCompare;
         this._aliases = {};
+        this._weight = 1;
     }
 
-    Roulette.prototype = {
-        /**
-         * Add an item to the collection
-         *
-         * @method add
-         * @chainable
-         * @param {any} item* Item(s) to add
-         */
-        add: function(item) {
-            if(arguments.length > 1) {
-                var autocal = this.autocalibrate;
-                if(autocal) this.autocalibrate = false; // turn off autocalibration during multiple additions
+    Roulette.select = function(list, weights) {
+    };
 
-                for(var i = 0, l = arguments.length; i < l; i++) {
-                    this.add(arguments[i]);
-                }
+    /**
+     * Add an item to the collection
+     *
+     * @method add
+     * @chainable
+     * @param {any} item* Item(s) to add
+     */
+    Roulette.prototype.add = function(item) {
+        if(arguments.length > 1) {
+            var autocal = this.autocalibrate;
+            if(autocal) this.autocalibrate = false; // turn off autocalibration during multiple additions
 
-                if(autocal) this.autocalibrate = true;
-            } else {
-                var idx = this.indexOf(item);
-
-                if(idx < 0) {
-                    // item doesn't exist
-                    this._data.push(makeItem(item, 1, 0));
-                    this.length = this._data.length;
-                    this._total++;
-
-                    // add alias if appropriate
-                    if(is('string', item) || is('number', item)) this._aliases[item] = this.length - 1;
-                } else {
-                    // item exists - just increase probability
-                    this._data[idx].tally++;
-                    this._total++;
-                }
-
-                if(this.autocalibrate) this.calibrate();
+            for(var i = 0, l = arguments.length; i < l; i++) {
+                this.add(arguments[i]);
             }
 
-            return this;
-        },
-
-        /**
-         * Decrease an item's probability. Remove if probability is zero
-         *
-         * @method remove
-         * @chainable
-         * @param {any} item* Item(s) to remove
-         */
-        remove: function(item) {
-            if(arguments.length > 1) {
-                var autocal = this.autocalibrate;
-                if(autocal) this.autocalibrate = false;
-
-                for(var i = 0, l = arguments.length; i < l; i++) {
-                    this.remove(arguments[i]);
-                }
-
-                if(autocal) this.autocalibrate = true;
-            } else {
-                var idx = this.indexOf(item);
-                if(idx < 0) return this;
-                if(this._data[idx].tally > 0) {
-                    this._data[idx].tally--;
-                    this._total--;
-                }
-
-                if(!this._data[idx].tally) {
-                    // item needs to be removed
-                    if(is('string', item) || is('number', item)) {
-                        delete this._aliases[item];
-                        this._realias(idx);
-                    }
-                    this._data.splice(idx, 1);
-                    this.length = this._data.length;
-                }
-
-                if(this.autocalibrate) this.calibrate();
+            if(autocal) {
+                this.autocalibrate = true;
+                this.calibrate();
             }
-
-            return this;
-        },
-
-        /**
-         * Remove an item entirely from the collection
-         *
-         * @method purge
-         * @chainable
-         * @param {any} item Item to remove
-         */
-        purge: function(item) {
+        } else {
             var idx = this.indexOf(item);
-            if(idx < 0) return this;
 
-            var n = this._data[idx].tally;
-            this._total -= n;
-            this._data.splice(idx, 1);
-            this.length = this._data.length;
-            if(is('string', item) || is('number', item)) {
-                delete this._aliases[item];
-                this._realias(idx);
+            if(idx < 0) {
+                // item doesn't exist
+                this._data.push(makeItem(item, 1, 0));
+                this.length = this._data.length;
+                this._total++;
+
+                // add alias if appropriate
+                if(is('string', item) || is('number', item)) this._aliases[item] = this.length - 1;
+            } else {
+                // item exists - just increase probability
+                this._data[idx].tally++; // increase item tally
+                this._total++; // increase total tally
             }
 
             if(this.autocalibrate) this.calibrate();
-
-            return this;
-        },
-
-        /**
-         * Reset aliases if applicable
-         *
-         * @method _realias
-         * @private
-         * @chainable
-         * @param {Number} [s] Index to start at
-         */
-        _realias: function(s) {
-            if(!is('number', s) || s < 0) s = 0;
-            if(s > this.length - 1) return this;
-            for(var i = s, l = this.length; i < l; i++) {
-                var item = this._data[i].item;
-                this._aliases[item] = i;
-            }
-            return this;
-        },
-
-        /**
-         * Search for an item and return its index or -1 if not found
-         *
-         * @method indexOf
-         * @param {any} item Item
-         * @return {Number} Item index or -1 if not found
-         * TODO: Linear search rulez
-         */
-        indexOf: function(item) {
-            if(!this.length) return -1;
-
-            if(is('string', item) || is('number', item))
-                return item in this._aliases ? this._aliases[item] : -1;
-
-            for (var i = 0, l = this.length; i < l; i ++) {
-                var e = this._data[i].item;
-                if(e == null) continue;
-                if(this._compare(item, e)) return i;
-            }
-
-            return -1;
-        },
-
-        /**
-         * Check if item exists
-         *
-         * @method has
-         * @param {any} item Item
-         * @return {Boolean} Whether item exists in collection
-         */
-        has: function(item) {
-            return !!(this.indexOf(item) >= 0);
-        },
-
-        /**
-         * Get an item by index or randomly by weight
-         *
-         * @method get
-         * @param {Number} [idx] Item
-         * @return {any} Item
-         * TODO: Linear search rulez agayne
-         */
-        get: function(idx) {
-            if(!this.length) return null;
-            if(is('number', idx)) {
-                if(idx >= 0 && idx < this.length) return this._data[idx].item;
-                return null;
-            }
-
-            var rn = Math.random();
-            if(rn >= 1) return this._data[this.length - 1].item;
-            if(rn <= 0) return this._data[0].item;
-            for (var i = 0, l = this.length; i < l; i ++) {
-                var prob = this._data[i].probability;
-                if(prob > rn) return this._data[i].item; // pick first item which exceeds throw
-            }
-
-            return null;
-        },
-
-        /**
-         * Adjust the probabilities
-         *
-         * @method calibrate
-         * @chainable
-         * TODO: is there any better way to do this?
-         */
-        calibrate: function() {
-            if(!this._total) return this;
-            for (var i = 0, l = this.length; i < l; i ++) {
-                var c = this._data[i].tally;
-                var prob = c / this._total;
-                this._data[i].probability = i ? this._data[i - 1].probability + prob : prob;
-            }
-            return this;
-        },
-
-        /**
-         * Set comparison function
-         *
-         * @method setCompare
-         * @chainable
-         * @param {Function} fn Comparison function
-         */
-        setCompare: function(fn) {
-            this._compare = fn;
-            return this;
         }
+
+        return this;
+    };
+
+    /**
+     * Decrease an item's probability. Remove if probability is zero
+     *
+     * @method remove
+     * @chainable
+     * @param {any} item* Item(s) to remove
+     */
+    Roulette.prototype.remove = function(item) {
+        if(arguments.length > 1) {
+            var autocal = this.autocalibrate;
+            if(autocal) this.autocalibrate = false;
+
+            for(var i = 0, l = arguments.length; i < l; i++) {
+                this.remove(arguments[i]);
+            }
+
+            if(autocal) {
+                this.autocalibrate = true;
+                this.calibrate();
+            }
+        } else {
+            var idx = this.indexOf(item);
+            if(idx < 0) return this;
+            if(this._data[idx].tally > 0) {
+                this._data[idx].tally--;
+                this._total--;
+            }
+
+            if(!this._data[idx].tally) {
+                this._data.splice(idx, 1);
+                this.length = this._data.length;
+
+                // item needs to be removed
+                if(is('string', item) || is('number', item)) {
+                    delete this._aliases[item];
+                    this._realias(idx);
+                }
+            }
+
+            if(this.autocalibrate) this.calibrate();
+        }
+
+        return this;
+    };
+
+    /**
+     * Remove an item entirely from the collection
+     *
+     * @method purge
+     * @chainable
+     * @param {any} item Item to remove
+     */
+    Roulette.prototype.purge = function(item) {
+        var idx = this.indexOf(item);
+        if(idx < 0) return this;
+
+        var n = this._data[idx].tally;
+        this._total -= n;
+        this._data.splice(idx, 1);
+        this.length = this._data.length;
+        if(is('string', item) || is('number', item)) {
+            delete this._aliases[item];
+            this._realias(idx);
+        }
+
+        if(this.autocalibrate) this.calibrate();
+
+        return this;
+    };
+
+    /**
+     * Reset aliases if applicable
+     *
+     * @method _realias
+     * @private
+     * @chainable
+     * @param {Number} [s] Index to start at
+     */
+    Roulette.prototype._realias = function(s) {
+        if(!is('number', s) || s < 0) s = 0;
+        if(s > this.length - 1) return this;
+        for(var i = s, l = this.length; i < l; i++) {
+            var item = this._data[i].item;
+            this._aliases[item] = i;
+        }
+        return this;
+    };
+
+    /**
+     * Search for an item and return its index or -1 if not found
+     *
+     * @method indexOf
+     * @param {any} item Item
+     * @return {Number} Item index or -1 if not found
+     * TODO: Linear search rulez
+     */
+    Roulette.prototype.indexOf = function(item) {
+        if(!this.length) return -1;
+
+        if(is('string', item) || is('number', item))
+            return item in this._aliases ? this._aliases[item] : -1;
+
+        for (var i = 0, l = this.length; i < l; i ++) {
+            var e = this._data[i].item;
+            if(e == null) continue;
+            if(this._compare(item, e)) return i;
+        }
+
+        return -1;
+    };
+
+    /**
+     * Check if item exists
+     *
+     * @method has
+     * @param {any} item Item
+     * @return {Boolean} Whether item exists in collection
+     */
+    Roulette.prototype.has = function(item) {
+        return !!(this.indexOf(item) >= 0);
+    };
+
+    /**
+     * Get an item randomly by weight
+     *
+     * @method get
+     * @param {Number} [weight] Weight adjustment
+     * @return {any} Item
+     * TODO: Linear search rulez agayne
+     */
+    Roulette.prototype.get = function(weight) {
+        if((typeof weight == "Number") && weight != this._weight) {
+            this.calibrate(weight);
+        }
+
+        var rn = Math.random();
+        if(rn >= 1) return this._data[this.length - 1].item;
+        if(rn <= 0) return this._data[0].item;
+        for (var i = 0, l = this.length; i < l; i ++) {
+            var prob = this._data[i].pcum;
+            console.log('prob of ' + this._data[i].item + ': ', prob);
+            if(prob > rn) return this._data[i].item; // pick first item which exceeds throw
+        }
+
+        return null;
+    };
+
+    /**
+     * Get an item by index
+     *
+     * @method index
+     * @param {Number} idx Item
+     * @return {any} Item
+     */
+    Roulette.prototype.index = function(idx) {
+        if(!this.length) return null;
+        if(is('number', idx)) {
+            if(idx >= 0 && idx < this.length) return this._data[idx].item;
+            return null;
+        }
+
+        return null;
+    };
+
+    /**
+     * Adjust the probabilities
+     *
+     * @method calibrate
+     * @chainable
+     * @param {Number} [weight] Adjust the weight of the probabilities
+     * generated (e.g. 0 is just random, 2 is double weighted). Default 1
+     */
+    Roulette.prototype.calibrate = function(weight) {
+        if(!this._total) return this;
+        if(typeof weight == "undefined") weight = 1;
+
+        if(weight < 0) weight = 0;
+        else if(weight > 100) weight = 100;
+
+        this._weight = weight; // record what weight we're using
+
+        var probabilities = [];
+
+        // get probabilities
+        for (var i = 0, l = this.length; i < l; i ++) {
+            var c = this._data[i].tally;
+            var prob = c / this._total;
+
+            probabilities[i] = prob;
+        }
+
+        // TODO: figure out scaling
+        for(var i = 0, l = this.length; i < l; i++) {
+            this._data[i].probability = prob;
+            this._data[i].pcum = i ? this._data[i - 1].pcum + prob : prob;
+        }
+
+        return this;
+    };
+
+    /**
+     * Set comparison function
+     *
+     * @method setCompare
+     * @chainable
+     * @param {Function} fn Comparison function
+     */
+    Roulette.prototype.setCompare = function(fn) {
+        this._compare = fn;
+        return this;
     };
 
     return Roulette;
